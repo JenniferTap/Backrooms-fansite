5294c39db0dddbf5b4eba5c43bf391e1c938a859
JenniferTap <jennifer.taphoorn@gmail.com>
2026-09-11 12:57:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,415 @@
+let lyrics1row1 = document.querySelector("#lyrics1row1");
+let lyrics1row2 = document.querySelector("#lyrics1row2");
+let lyrics1row3 = document.querySelector("#lyrics1row3");
+let lyrics1row4 = document.querySelector("#lyrics1row4");
+let lyrics1row5 = document.querySelector("#lyrics1row5");
+let lyrics1row6 = document.querySelector("#lyrics1row6");
+let lyrics1row7 = document.querySelector("#lyrics1row7");
+let lyrics1row8 = document.querySelector("#lyrics1row8");
+let lyrics2row1 = document.querySelector("#lyrics2row1");
+let lyrics2row2 = document.querySelector("#lyrics2row2");
+let lyrics2row3 = document.querySelector("#lyrics2row3");
+let lyrics2row4 = document.querySelector("#lyrics2row4");
+let lyrics2row5 = document.querySelector("#lyrics2row5");
+let lyrics2row6 = document.querySelector("#lyrics2row6");
+let lyrics2row7 = document.querySelector("#lyrics2row7");
+let lyrics2row8 = document.querySelector("#lyrics2row8");
+let lyrics2row9 = document.querySelector("#lyrics2row9");
+let lyrics2row10 = document.querySelector("#lyrics2row10");
+let lyrics2row11 = document.querySelector("#lyrics2row11");
+let lyrics2row12 = document.querySelector("#lyrics2row12");
+let lyrics2row13 = document.querySelector("#lyrics2row13");
+let lyrics2row14 = document.querySelector("#lyrics2row14");
+let lyrics3row1 = document.querySelector("#lyrics3row1");
+let lyrics3row2 = document.querySelector("#lyrics3row2");
+let lyrics3row3 = document.querySelector("#lyrics3row3");
+let lyrics3row4 = document.querySelector("#lyrics3row4");
+let lyrics3row5 = document.querySelector("#lyrics3row5");
+let lyrics3row6 = document.querySelector("#lyrics3row6");
+let lyrics3row7 = document.querySelector("#lyrics3row7");
+let lyrics3row8 = document.querySelector("#lyrics3row8");
+let lyrics3row9 = document.querySelector("#lyrics3row9");
+let lyrics3row10 = document.querySelector("#lyrics3row10");
+let lyrics3row11 = document.querySelector("#lyrics3row11");
+
+
+//LYRICS 1
+lyrics1row1.onmouseover = function() {
+    lyrics1row1.innerHTML = 'Removed the cartridge';
+    lyrics1row1.style.color = 'red';
+};
+
+lyrics1row1.onmouseout = function() {
+    lyrics1row1.innerHTML = '引き抜くカセット';
+    lyrics1row1.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row2.onmouseover = function() {
+    lyrics1row2.innerHTML = 'Pressed the reset button';
+    lyrics1row2.style.color = 'red';
+};
+
+lyrics1row2.onmouseout = function() {
+    lyrics1row2.innerHTML = '押し込むリセット';
+    lyrics1row2.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row3.onmouseover = function() {
+    lyrics1row3.innerHTML = 'Stared into the maze';
+    lyrics1row3.style.color = 'red';
+};
+
+lyrics1row3.onmouseout = function() {
+    lyrics1row3.innerHTML = '迷い込む迷路';
+    lyrics1row3.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row4.onmouseover = function() {
+    lyrics1row4.innerHTML = 'Moved forward, but "dal segno"';
+    lyrics1row4.style.color = 'red';
+};
+
+lyrics1row4.onmouseout = function() {
+    lyrics1row4.innerHTML = '進めどもダルセーニョ';
+    lyrics1row4.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row5.onmouseover = function() {
+    lyrics1row5.innerHTML = 'I saw my own back beyond the doors';
+    lyrics1row5.style.color = 'red';
+};
+
+lyrics1row5.onmouseout = function() {
+    lyrics1row5.innerHTML = 'ドアの先に僕の背中が見えた';
+    lyrics1row5.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row6.onmouseover = function() {
+    lyrics1row6.innerHTML = 'I saw hope when I turned around';
+    lyrics1row6.style.color = 'red';
+};
+
+lyrics1row6.onmouseout = function() {
+    lyrics1row6.innerHTML = '振り向いた先に希望が見えた';
+    lyrics1row6.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row7.onmouseover = function() {
+    lyrics1row7.innerHTML = 'Where is the end of this hell?';
+    lyrics1row7.style.color = 'red';
+};
+
+lyrics1row7.onmouseout = function() {
+    lyrics1row7.innerHTML = '地獄の果てなどどこにあるのか';
+    lyrics1row7.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics1row8.onmouseover = function() {
+    lyrics1row8.innerHTML = 'Is there really no exit?';
+    lyrics1row8.style.color = 'red';
+};
+
+lyrics1row8.onmouseout = function() {
+    lyrics1row8.innerHTML = '出口はまだなの？';
+    lyrics1row8.style.color = 'rgb(61, 50, 2)';
+};
+
+// END LYRICS 1
+
+// LYRICS 2
+
+lyrics2row1.onmouseover = function() {
+    lyrics2row1.innerHTML = 'spinning, spinning, spinning, spinning';
+    lyrics2row1.style.color = 'red';
+};
+
+lyrics2row1.onmouseout = function() {
+    lyrics2row1.innerHTML = 'くるくるくるくる';
+    lyrics2row1.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row2.onmouseover = function() {
+    lyrics2row2.innerHTML = 'Spinning round and round,Spinning round and round';
+    lyrics2row2.style.color = 'red';
+};
+
+lyrics2row2.onmouseout = function() {
+    lyrics2row2.innerHTML = 'くりかえす、くりかえす';
+    lyrics2row2.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row3.onmouseover = function() {
+    lyrics2row3.innerHTML = 'Spinning round and round, Spinning round and round,';
+    lyrics2row3.style.color = 'red';
+};
+
+lyrics2row3.onmouseout = function() {
+    lyrics2row3.innerHTML = 'くりかえす、くりかえす';
+    lyrics2row3.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row4.onmouseover = function() {
+    lyrics2row4.innerHTML = 'Dizzy, dizzy, dizzy, dizzy,';
+    lyrics2row4.style.color = 'red';
+};
+
+lyrics2row4.onmouseout = function() {
+    lyrics2row4.innerHTML = 'ふらふらふらふら ';
+    lyrics2row4.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row5.onmouseover = function() {
+    lyrics2row5.innerHTML = 'dazed and fracturing, dazed and fracturing';
+    lyrics2row5.style.color = 'red';
+};
+
+lyrics2row5.onmouseout = function() {
+    lyrics2row5.innerHTML = 'ふらくたる、ふらくたる';
+    lyrics2row5.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row6.onmouseover = function() {
+    lyrics2row6.innerHTML = 'dazed and fracturing, dazed and fracturing';
+    lyrics2row6.style.color = 'red';
+};
+
+lyrics2row6.onmouseout = function() {
+    lyrics2row6.innerHTML = 'ふらくたる、ふらくたる';
+    lyrics2row6.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row7.onmouseover = function() {
+    lyrics2row7.innerHTML = 'Spinning my thoughts';
+    lyrics2row7.style.color = 'red';
+};
+
+lyrics2row7.onmouseout = function() {
+    lyrics2row7.innerHTML = '空回るソウト';
+    lyrics2row7.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row8.onmouseover = function() {
+    lyrics2row8.innerHTML = 'An endless road';
+    lyrics2row8.style.color = 'red';
+};
+
+lyrics2row8.onmouseout = function() {
+    lyrics2row8.innerHTML = '終わらないロード';
+    lyrics2row8.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row9.onmouseover = function() {
+    lyrics2row9.innerHTML = 'Iron-scented oxygen';
+    lyrics2row9.style.color = 'red';
+};
+
+lyrics2row9.onmouseout = function() {
+    lyrics2row9.innerHTML = '鉄臭い酸素';
+    lyrics2row9.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row10.onmouseover = function() {
+    lyrics2row10.innerHTML = 'An unheard SOS';
+    lyrics2row10.style.color = 'red';
+};
+
+lyrics2row10.onmouseout = function() {
+    lyrics2row10.innerHTML = '届かないSOS';
+    lyrics2row10.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row11.onmouseover = function() {
+    lyrics2row11.innerHTML = 'I tided over hunger eating pomеgranates fallen on the road';
+    lyrics2row11.style.color = 'red';
+};
+
+lyrics2row11.onmouseout = function() {
+    lyrics2row11.innerHTML = '落ちてた柘榴で飢えを凌いだ';
+    lyrics2row11.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row12.onmouseover = function() {
+    lyrics2row12.innerHTML = 'I hеard tinnitus inside my skull';
+    lyrics2row12.style.color = 'red';
+};
+
+lyrics2row12.onmouseout = function() {
+    lyrics2row12.innerHTML = '頭蓋の中から耳鳴りがした';
+    lyrics2row12.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row13.onmouseover = function() {
+    lyrics2row13.innerHTML = 'If nothing changes no matter how far I go';
+    lyrics2row13.style.color = 'red';
+};
+
+lyrics2row13.onmouseout = function() {
+    lyrics2row13.innerHTML = '進めど進めど変わりないなら';
+    lyrics2row13.style.color = 'rgb(61, 50, 2)';
+};
+// end
+
+lyrics2row14.onmouseover = function() {
+    lyrics2row14.innerHTML = 'What meaning is there in resisting?';
+    lyrics2row14.style.color = 'red';
+};
+
+lyrics2row14.onmouseout = function() {
+    lyrics2row14.innerHTML = '抗う意味など';
+    lyrics2row14.style.color = 'rgb(61, 50, 2)';
+};
+// END LYRICS 2
+
+//LYRICS 3
+
+lyrics3row1.onmouseover = function() {
+    lyrics3row1.innerHTML = 'Spinning, spinning, spinning, spinning,';
+    lyrics3row1.style.color = 'red';
+};
+
+lyrics3row1.onmouseout = function() {
+    lyrics3row1.innerHTML = 'くるくるくるくる';
+    lyrics3row1.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row2.onmouseover = function() {
+    lyrics3row2.innerHTML = 'Spinning round and round, Spinning round and round,';
+    lyrics3row2.style.color = 'red';
+};
+
+lyrics3row2.onmouseout = function() {
+    lyrics3row2.innerHTML = 'くりかえす、くりかえす';
+    lyrics3row2.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row3.onmouseover = function() {
+    lyrics3row3.innerHTML = 'Spinning round and round, Spinning round and round,';
+    lyrics3row3.style.color = 'red';
+};
+
+lyrics3row3.onmouseout = function() {
+    lyrics3row3.innerHTML = 'くりかえす、くりかえす';
+    lyrics3row3.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row4.onmouseover = function() {
+    lyrics3row4.innerHTML = 'Dizzy, dizzy, dizzy, dizzy,';
+    lyrics3row4.style.color = 'red';
+};
+
+lyrics3row4.onmouseout = function() {
+    lyrics3row4.innerHTML = 'ふらふらふらふら';
+    lyrics3row4.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row5.onmouseover = function() {
+    lyrics3row5.innerHTML = 'dazed and fracturing, dazed and fracturing ';
+    lyrics3row5.style.color = 'red';
+};
+
+lyrics3row5.onmouseout = function() {
+    lyrics3row5.innerHTML = 'ふらくたる、ふらくたる';
+    lyrics3row5.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row6.onmouseover = function() {
+    lyrics3row6.innerHTML = 'dazed and fracturing, dazed and fracturing';
+    lyrics3row6.style.color = 'red';
+};
+
+lyrics3row6.onmouseout = function() {
+    lyrics3row6.innerHTML = 'ふらくたる、ふらくたる';
+    lyrics3row6.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row7.onmouseover = function() {
+    lyrics3row7.innerHTML = 'Ah, opened the doors';
+    lyrics3row7.style.color = 'red';
+};
+
+lyrics3row7.onmouseout = function() {
+    lyrics3row7.innerHTML = 'ああ、扉を開け';
+    lyrics3row7.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row8.onmouseover = function() {
+    lyrics3row8.innerHTML = 'Forgot the meaning';
+    lyrics3row8.style.color = 'red';
+};
+
+lyrics3row8.onmouseout = function() {
+    lyrics3row8.innerHTML = '意味も忘れ';
+    lyrics3row8.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row9.onmouseover = function() {
+    lyrics3row9.innerHTML = 'Into the depths of a mad dream';
+    lyrics3row9.style.color = 'red';
+};
+
+lyrics3row9.onmouseout = function() {
+    lyrics3row9.innerHTML = 'いかれた夢の奥へ';
+    lyrics3row9.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row10.onmouseover = function() {
+    lyrics3row10.innerHTML = 'Continuing on and on';
+    lyrics3row10.style.color = 'red';
+};
+
+lyrics3row10.onmouseout = function() {
+    lyrics3row10.innerHTML = 'どこまでも続いてく';
+    lyrics3row10.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
+lyrics3row11.onmouseover = function() {
+    lyrics3row11.innerHTML = 'Round and round';
+    lyrics3row11.style.color = 'red';
+};
+
+lyrics3row11.onmouseout = function() {
+    lyrics3row11.innerHTML = 'くるくるくりかえす';
+    lyrics3row11.style.color = 'rgb(61, 50, 2)';
+};
+
+//end
+
